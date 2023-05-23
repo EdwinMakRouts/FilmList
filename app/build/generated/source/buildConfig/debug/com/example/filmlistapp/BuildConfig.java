76770c288262e2a567f115aa6d51bbeabe0696ba
Edwin Makoveev Routskaia <makoveevedwin@gmail.com>
2023-05-23 03:49:12 +0200

    a

--- app/build/generated/source/buildConfig/debug/com/example/filmlistapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/filmlistapp/BuildConfig.java
@@ -9,4 +9,6 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String API = "4891180565ace4dd9df2871532e7f3ea";
 }
